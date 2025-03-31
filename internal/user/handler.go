c5544bb364f086db0338722cb42e311164369372
Nariman Ibadullaev <n.ibadullaev@inc4.net>
2025-03-31 16:01:16 +0400

    fix: write mongo operations

--- internal/user/handler.go
+++ internal/user/handler.go
@@ -8,6 +8,8 @@ import (
 
 	"github.com/julienschmidt/httprouter"
 	"github.com/sirupsen/logrus"
+	"go.mongodb.org/mongo-driver/bson/primitive"
+	"go.mongodb.org/mongo-driver/mongo"
 )
 
 var _ handlers.Handler = &handler{}
@@ -40,8 +42,20 @@ func (h *handler) Register(router *httprouter.Router) {
 
 func (h *handler) GetList(w http.ResponseWriter, r *http.Request, params httprouter.Params) {
 	h.logger.Info("GetList called for users")
-	w.WriteHeader(200)
-	w.Write([]byte("this is list of users"))
+
+	users, err := h.storage.GetAll(r.Context())
+	if err != nil {
+		h.logger.Errorf("Failed to get users: %v", err)
+		http.Error(w, "failed to get users", http.StatusInternalServerError)
+		return
+	}
+
+	w.Header().Set("Content-Type", "application/json")
+	w.WriteHeader(http.StatusOK)
+	if err := json.NewEncoder(w).Encode(users); err != nil {
+		h.logger.Errorf("Failed to encode users list: %v", err)
+		http.Error(w, "failed to encode users", http.StatusInternalServerError)
+	}
 }
 
 func (h *handler) CreateUser(w http.ResponseWriter, r *http.Request, params httprouter.Params) {
@@ -60,18 +74,101 @@ func (h *handler) CreateUser(w http.ResponseWriter, r *http.Request, params http
 }
 
 func (h *handler) GetUserByUUID(w http.ResponseWriter, r *http.Request, params httprouter.Params) {
-	w.WriteHeader(200)
-	w.Write([]byte("this is get the user"))
+	h.logger.Info("GetUserByUUID called for user")
+
+	id := params.ByName("uuid")
+
+	user, err := h.storage.FindOne(r.Context(), id)
+	if err != nil {
+		h.logger.Errorf("Failed to find user by ID %s: %v", id, err)
+		if err == mongo.ErrNoDocuments {
+			http.Error(w, "user not found", http.StatusNotFound)
+		} else {
+			http.Error(w, "failed to get user", http.StatusInternalServerError)
+		}
+		return
+	}
+
+	w.Header().Set("Content-Type", "application/json")
+	w.WriteHeader(http.StatusOK)
+	if err := json.NewEncoder(w).Encode(user); err != nil {
+		h.logger.Errorf("Failed to encode user: %v", err)
+		http.Error(w, "failed to encode user", http.StatusInternalServerError)
+	}
 }
+
 func (h *handler) UpdateUser(w http.ResponseWriter, r *http.Request, params httprouter.Params) {
-	w.WriteHeader(204)
-	w.Write([]byte("this is update the user"))
+	h.logger.Info("UpdateUser called for user")
+
+	id := params.ByName("uuid")
+	h.logger.Infof("Attempting to update user with id: %s", id)
+
+	var user User
+	if err := json.NewDecoder(r.Body).Decode(&user); err != nil {
+		h.logger.Errorf("Invalid request body: %v", err)
+		http.Error(w, "invalid request body", http.StatusBadRequest)
+		return
+	}
+
+	user.ID = id
+
+	h.logger.Infof("User data to be updated: %+v", user)
+
+	err := h.storage.Update(r.Context(), user)
+	if err != nil {
+		h.logger.Errorf("Failed to update user %s: %v", id, err)
+		http.Error(w, "failed to update user", http.StatusInternalServerError)
+		return
+	}
+
+	h.logger.Info("User updated successfully")
+	w.WriteHeader(http.StatusNoContent)
 }
+
 func (h *handler) PartiallyUpdateUser(w http.ResponseWriter, r *http.Request, params httprouter.Params) {
-	w.WriteHeader(204)
-	w.Write([]byte("this is partially update the user"))
+	h.logger.Info("PartiallyUpdateUser called for user")
+
+	id := params.ByName("uuid")
+
+	var user User
+	if err := json.NewDecoder(r.Body).Decode(&user); err != nil {
+		h.logger.Errorf("Invalid request body: %v", err)
+		http.Error(w, "invalid request body", http.StatusBadRequest)
+		return
+	}
+
+	user.ID = id
+
+	err := h.storage.PartiallyUpdate(r.Context(), user)
+	if err != nil {
+		h.logger.Errorf("Failed to partially update user %s: %v", id, err)
+		http.Error(w, "failed to partially update user", http.StatusInternalServerError)
+		return
+	}
+
+	w.WriteHeader(http.StatusNoContent)
 }
+
 func (h *handler) DeleteUser(w http.ResponseWriter, r *http.Request, params httprouter.Params) {
-	w.WriteHeader(204)
-	w.Write([]byte("this is delete the user"))
+	id := params.ByName("uuid")
+	h.logger.Infof("Attempting to delete user with id: %s", id)
+
+	objectID, err := primitive.ObjectIDFromHex(id)
+	if err != nil {
+		h.logger.Errorf("Invalid UUID format: %v", err)
+		http.Error(w, "invalid UUID format", http.StatusBadRequest)
+		return
+	}
+
+	idString := objectID.Hex()
+
+	err = h.storage.Delete(r.Context(), idString)
+	if err != nil {
+		h.logger.Errorf("Failed to delete user %s: %v", id, err)
+		http.Error(w, "failed to delete user", http.StatusInternalServerError)
+		return
+	}
+
+	h.logger.Infof("User %s deleted successfully", id)
+	w.WriteHeader(http.StatusNoContent)
 }
--- internal/user/storage.go
+++ internal/user/storage.go
@@ -13,6 +13,8 @@ type Storage interface {
 	FindOne(ctx context.Context, id string) (User, error)
 	Update(ctx context.Context, user User) error
 	Delete(ctx context.Context, id string) error
+	GetAll(ctx context.Context) ([]User, error)
+	PartiallyUpdate(ctx context.Context, user User) error
 }
 
 type MongoStorage struct {
@@ -25,36 +27,107 @@ func NewMongoStorage(client *mongo.Client, dbName, collectionName string) *Mongo
 	}
 }
 
+func (s *MongoStorage) GetAll(ctx context.Context) ([]User, error) {
+	cursor, err := s.collection.Find(ctx, bson.M{})
+	if err != nil {
+		return nil, err
+	}
+	defer cursor.Close(ctx)
+
+	var users []User
+	for cursor.Next(ctx) {
+		var user User
+		if err := cursor.Decode(&user); err != nil {
+			return nil, err
+		}
+		users = append(users, user)
+	}
+
+	if err := cursor.Err(); err != nil {
+		return nil, err
+	}
+
+	return users, nil
+}
+
 func (s *MongoStorage) Create(ctx context.Context, user User) (string, error) {
 	res, err := s.collection.InsertOne(ctx, user)
 	if err != nil {
 		return "", err
 	}
 
-	// Преобразуем InsertedID в строку
 	id, ok := res.InsertedID.(primitive.ObjectID)
 	if !ok {
 		return "", mongo.ErrNilDocument
 	}
 
-	return id.Hex(), nil // Преобразуем ObjectID в строку
+	return id.Hex(), nil
 }
 
 func (s *MongoStorage) FindOne(ctx context.Context, id string) (User, error) {
 	var user User
-	err := s.collection.FindOne(ctx, bson.M{"_id": id}).Decode(&user)
+	objectID, err := primitive.ObjectIDFromHex(id)
 	if err != nil {
-		return User{}, err
+		return user, err
 	}
+
+	err = s.collection.FindOne(ctx, bson.M{"_id": objectID}).Decode(&user)
+	if err != nil {
+		return user, err
+	}
+
 	return user, nil
 }
 
 func (s *MongoStorage) Update(ctx context.Context, user User) error {
-	_, err := s.collection.UpdateOne(ctx, bson.M{"_id": user.ID}, bson.M{"$set": user})
+	objectID, err := primitive.ObjectIDFromHex(user.ID)
+	if err != nil {
+		return err
+	}
+
+	_, err = s.collection.UpdateOne(
+		ctx,
+		bson.M{"_id": objectID},
+		bson.M{"$set": bson.M{
+			"email":    user.Email,
+			"username": user.Username,
+			"password": user.PasswordHash,
+		}},
+	)
+	return err
+}
+
+func (s *MongoStorage) PartiallyUpdate(ctx context.Context, user User) error {
+	objectID, err := primitive.ObjectIDFromHex(user.ID)
+	if err != nil {
+		return err
+	}
+
+	updateFields := bson.M{}
+	if user.Email != "" {
+		updateFields["email"] = user.Email
+	}
+	if user.Username != "" {
+		updateFields["username"] = user.Username
+	}
+	if user.PasswordHash != "" {
+		updateFields["password"] = user.PasswordHash
+	}
+
+	_, err = s.collection.UpdateOne(
+		ctx,
+		bson.M{"_id": objectID},
+		bson.M{"$set": updateFields},
+	)
 	return err
 }
 
 func (s *MongoStorage) Delete(ctx context.Context, id string) error {
-	_, err := s.collection.DeleteOne(ctx, bson.M{"_id": id})
+	objectID, err := primitive.ObjectIDFromHex(id)
+	if err != nil {
+		return err
+	}
+
+	_, err = s.collection.DeleteOne(ctx, bson.M{"_id": objectID})
 	return err
 }
