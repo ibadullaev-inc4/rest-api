934bb54acb1e6e4afe21b6f4e1637eaad887b2fd
Nariman Ibadullaev <n.ibadullaev@inc4.net>
2025-04-03 17:51:23 +0400

    fix: add apperror midleware

--- internal/admin/handler.go
+++ internal/admin/handler.go
@@ -2,6 +2,7 @@ package admin
 
 import (
 	"net/http"
+	"rest-api/internal/apperror"
 	"rest-api/internal/handlers"
 
 	"github.com/julienschmidt/httprouter"
@@ -22,36 +23,39 @@ func NewHandler() handlers.Handler {
 }
 
 func (h *handler) Register(router *httprouter.Router) {
-	router.GET(usersURL, h.GetList)
-	router.POST(usersURL, h.CreateUser)
-	router.GET(userURL, h.GetUserByUUID)
-	router.PUT(userURL, h.UpdateUser)
-	router.PATCH(userURL, h.PartiallyUpdateUser)
-	router.DELETE(userURL, h.DeleteUser)
+	router.HandlerFunc(http.MethodGet, usersURL, apperror.ErrorMiddleware(h.GetList))
+	router.HandlerFunc(http.MethodPost, usersURL, apperror.ErrorMiddleware(h.CreateUser))
+	router.HandlerFunc(http.MethodGet, userURL, apperror.ErrorMiddleware(h.GetUserByUUID))
+	router.HandlerFunc(http.MethodPut, userURL, apperror.ErrorMiddleware(h.UpdateUser))
+	router.HandlerFunc(http.MethodPatch, userURL, apperror.ErrorMiddleware(h.PartiallyUpdateUser))
+	router.HandlerFunc(http.MethodDelete, userURL, apperror.ErrorMiddleware(h.DeleteUser))
 }
 
-func (h *handler) GetList(w http.ResponseWriter, r *http.Request, params httprouter.Params) {
-	w.WriteHeader(200)
-	w.Write([]byte("this is list of admins"))
+func (h *handler) GetList(w http.ResponseWriter, r *http.Request) error {
+	return apperror.ErrNotFound
 }
-
-func (h *handler) CreateUser(w http.ResponseWriter, r *http.Request, params httprouter.Params) {
+func (h *handler) CreateUser(w http.ResponseWriter, r *http.Request) error {
 	w.WriteHeader(201)
 	w.Write([]byte("this is create the admin"))
+	return nil
 }
-func (h *handler) GetUserByUUID(w http.ResponseWriter, r *http.Request, params httprouter.Params) {
+func (h *handler) GetUserByUUID(w http.ResponseWriter, r *http.Request) error {
 	w.WriteHeader(200)
 	w.Write([]byte("this is get the admin"))
+	return nil
 }
-func (h *handler) UpdateUser(w http.ResponseWriter, r *http.Request, params httprouter.Params) {
+func (h *handler) UpdateUser(w http.ResponseWriter, r *http.Request) error {
 	w.WriteHeader(204)
 	w.Write([]byte("this is update the admin"))
+	return nil
 }
-func (h *handler) PartiallyUpdateUser(w http.ResponseWriter, r *http.Request, params httprouter.Params) {
+func (h *handler) PartiallyUpdateUser(w http.ResponseWriter, r *http.Request) error {
 	w.WriteHeader(204)
 	w.Write([]byte("this is partially update the admin"))
+	return nil
 }
-func (h *handler) DeleteUser(w http.ResponseWriter, r *http.Request, params httprouter.Params) {
+func (h *handler) DeleteUser(w http.ResponseWriter, r *http.Request) error {
 	w.WriteHeader(204)
 	w.Write([]byte("this is delete the admin"))
+	return nil
 }
